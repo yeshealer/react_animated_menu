60e39e329ac08d86a6c55288cbdf4e184d13937c
dosin <topfirepassion@gmail.com>
2021-11-29 08:31:18 +0100

    react animated menu

---
+++ src/app/StyledApp.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const StyledApp = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  align-content: center;
+`
+
+StyledApp.displayName = 'StyledApp'
---
+++ src/app/StyledBurgerContainer.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const StyledBurgerContainer = styled.div`
+  width: 240px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+`
+
+StyledBurgerContainer.displayName = 'StyledBurgerContainer'
